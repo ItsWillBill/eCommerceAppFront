a5b14ed9779a840c3bbb3703a833bab5530c9d61
Will <bill.williams.tsofack.fozeing@efrei.net>
2022-11-28 17:03:25 +0100

    get and display product list from springboot rest api

---
+++ src/app/common/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    sku: string,
+    name: string,
+    description: string,
+    unitPrice: number,
+    active: boolean,
+    unitsInStock: number,
+    dateCreated: Date,
+    lastUpdated: Date    
+}
