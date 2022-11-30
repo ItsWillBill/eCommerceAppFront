15a971c1cdd93d3ef906ec73d3e60956c48597bb
Will <bill.williams.tsofack.fozeing@efrei.net>
2022-11-30 16:09:06 +0100

    Add pagination

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,7 +12,15 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   searchMode: boolean = false;
+
+  // new properties for pagination
+  pageNumber: number = 1;
+  pageSize: number = 5;
+  totalElements: number = 0;
+
+  previousKeyword: string = "";
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -44,23 +52,59 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
     }
 
+    //
+    // check if we have a different category than previous
+    // NOTE: Anguar will reuse a component if it's currently being viewed
+    //
+
+    // if we have a different category id than previous
+    // then set pageNumber back to 1
+
+    if (this.previousCategoryId != this.currentCategoryId) {
+      this.pageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    console.log(`currentCategoryId = ${this.currentCategoryId}, pageNumber=${this.pageNumber}`);
+
     // now get the products for the givent category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    this.productService.getProductListPaginated(this.pageNumber-1, 
+      this.pageSize, this.currentCategoryId).subscribe(this.processResult())
   }
 
   handleSearchProducts() {
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
 
+    // if we have a different keyword than previous
+    // then set pageNumber to 1
+
+    if (this.previousKeyword != theKeyword) {
+      this.pageNumber = 1;
+    }
+
+    this.previousKeyword = theKeyword;
+
+    console.log(`keyword=${theKeyword}, pageNumber=${this.pageNumber}`);
+
     // now search for the producs using keyword
-    this.productService.searchProducts(theKeyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    this.productService.searchProductsPaginated(
+      this.pageNumber -1, this.pageSize, theKeyword).subscribe(this.processResult());
   }
 
+  updatePageSize(pageSize: string) {
+    this.pageSize = +pageSize;
+    this.pageNumber = 1;
+    this.listProducts();
+  }
+
+  processResult() {
+    return (data: any) => {
+      this.products = data._embedded.products;
+      this.pageNumber = data.page.number + 1;
+      this.pageSize = data.page.size;
+      this.totalElements = data.page.totalElements;
+    };
+  }
 }
+
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -28,9 +28,25 @@ export class ProductService {
     return this.httpClient.get<Product>(productUrl);
   }
 
+  getProductListPaginated(page: number, pageSize: number, categoryId: number):Observable<GetResponseProducts> { 
+
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`
+                    + `&page=${page}&size=${pageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   searchProducts(theKeyword: string):Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
     return this.getProducts(searchUrl);
+  }
+
+  searchProductsPaginated(page: number, pageSize: number, keyword: string):Observable<GetResponseProducts> { 
+
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${keyword}`
+                    + `&page=${page}&size=${pageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
   private getProducts(searchUrl: string): Observable<Product[]> {
@@ -48,12 +64,18 @@ export class ProductService {
 
 interface GetResponseProducts {
   _embedded: {
-    products : Product[];
+    products : Product[]
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
 interface GetResponseProductCategory {
   _embedded: {
-    productCategory : ProductCategory[];
+    productCategory : ProductCategory[]
   }
 }
